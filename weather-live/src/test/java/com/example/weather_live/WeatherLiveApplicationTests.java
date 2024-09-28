dc8aa66cc36cd0fbd28dd8ac99e1226f4307fc5e
AvnitShaw <avnitshaw@gmail.com>
2024-09-28 19:17:04 +0530

    created springboot base file using springboot site

---
+++ weather-live/src/test/java/com/example/weather_live/WeatherLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weather_live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
