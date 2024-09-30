99e5a6a4ab2ce3b87048e209769d479bae724cbb
AvnitShaw <avnitshaw@gmail.com>
2024-09-30 19:42:14 +0530

    modified

--- weather-live/src/main/java/com/example/weather_live/WeatherController.java
+++ weather-live/src/main/java/com/example/weather_live/WeatherController.java
@@ -1,6 +1,5 @@
 package com.example.weather_live;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class WeatherController {
 
-    @Autowired
     private WeatherService weatherService;
 
     @GetMapping("/")
